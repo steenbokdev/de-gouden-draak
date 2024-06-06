b0384c813a98f3a181503779c0e3fb87bbfd4db2
steenbokdev <larsbtech@outlook.com>
2024-06-06 12:22:40 +0200

    feat(component): add go-back

---
+++ resources/views/components/go-back.blade.php
@@ -0,0 +1,10 @@
+<a href="{{ $route }}">
+    <span class="icon-text">
+        <span class="icon">
+          <i class="fas fa-arrow-left"></i>
+        </span>
+        <span>
+            {{ $label }}
+        </span>
+    </span>
+</a>

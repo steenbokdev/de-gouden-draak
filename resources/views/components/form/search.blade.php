8cf15df4f8ac6fe5800b30f16ca87464d95005ad
steenbokdev <larsbtech@outlook.com>
2024-06-06 16:57:04 +0200

    feat(component): add form search

---
+++ resources/views/components/form/search.blade.php
@@ -0,0 +1,10 @@
+<div class="field has-addons">
+    <div class="control">
+        <input class="input" name="{{ $id }}" type="{{ $type }}" placeholder="{{ $placeholder }}" value="{{ $value }}">
+    </div>
+    <div class="control">
+        <button type="submit" class="button is-primary">
+            {{ __('form/shared.search') }}
+        </button>
+    </div>
+</div>

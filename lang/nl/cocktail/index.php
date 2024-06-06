36d503e3e6e4235fc1e8988efd8209801fe3247c
steenbokdev <larsbtech@outlook.com>
2024-06-06 16:57:43 +0200

    feat(cocktail): add search and filter

--- lang/nl/cocktail/index.php
+++ lang/nl/cocktail/index.php
@@ -5,5 +5,27 @@ return [
     'failed' => [
         'message' => 'Er is iets misgegaan bij het ophalen van de cocktails!',
         'try_again' => 'Probeer opnieuw'
+    ],
+    'search' => 'Zoek naar cocktails',
+    'filter' => [
+        'category' => [
+            'ordinary_drink' => 'Normaal',
+            'cocktail' => 'Cocktail',
+            'shake' => 'Shake',
+            'other_and_unknown' => 'Anders / Onbekend',
+            'cocoa' => 'Cacao',
+            'shot' => 'Shot',
+            'coffee_and_tea' => 'Koffie of Thee',
+            'homemade_liqueur' => 'Zelfgemaakte Likeur',
+            'punch_and_party_drink' => 'Bowl / Feestdrankje',
+            'beer' => 'Bier',
+            'soft_drink' => 'Frisdrank'
+        ],
+        'alcoholic' => [
+            'alcoholic' => 'Alcoholisch',
+            'non_alcoholic' => 'Niet Alcoholisch',
+            'optional_alcohol' => 'Optioneel Alcoholisch'
+        ],
+        'apply' => 'Toepassen'
     ]
 ];
--- resources/views/cocktail/index.blade.php
+++ resources/views/cocktail/index.blade.php
@@ -2,6 +2,38 @@
     'title' => __('cocktail/index.page_title')
 ])
 
+@section('addons-left')
+    <form action="{{ route('cocktail.index') }}" method="GET">
+        <x-form.search id="search" route="{{ route('cocktail.index') }}" type="text" placeholder="{{ __('cocktail/index.search') }}" value="{{ $searchQuery }}"/>
+    </form>
+@endsection
+
+@section('addons-right')
+    <form action="{{ route('cocktail.index') }}" method="GET">
+        <div class="field is-grouped">
+            <x-form.select id="category">
+                @foreach ($categories['collection'] as $key => $value)
+                    <option value="{{ $key }}" @selected($key === $categories['selected'])>
+                        {{ $value }}
+                    </option>
+                @endforeach
+            </x-form.select>
+        
+            <x-form.select id="alcoholic">
+                @foreach ($alcoholics['collection'] as $key => $value)
+                    <option value="{{ $key }}" @selected($key === $alcoholics['selected'])>
+                        {{ $value }}
+                    </option>
+                @endforeach
+            </x-form.select>
+
+            <button type="submit" class="button is-primary">
+                {{ __('cocktail/index.filter.apply') }}
+            </button>
+        </div>
+    </form>
+@endsection
+
 @section('content')
     <div class="fixed-grid has-3-cols">
         <div class="grid">

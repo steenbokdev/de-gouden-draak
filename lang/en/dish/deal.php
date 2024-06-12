06fc67c6fc77f589a0165ee6c9ed9b281bf3d803
steenbokdev <larsbtech@outlook.com>
2024-06-12 22:51:18 +0200

    refactor(lang): remove uppercased letter

--- lang/en/dish/deal.php
+++ lang/en/dish/deal.php
@@ -3,7 +3,7 @@
 return [
     'page_title' => 'Deals',
     'week' => 'Week',
-    'sale_price' => 'Sale Price',
+    'sale_price' => 'Sale price',
     'revoke' => 'Revoke',
     'no_deals_found' => 'No deals found',
     'dish' => 'Dish',

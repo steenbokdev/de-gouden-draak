804b632d7b457327c4519169555f9524a62e466b
steenbokdev <larsbtech@outlook.com>
2024-06-12 23:05:44 +0200

    refactor(view): cleanup lang view

--- resources/views/layouts/partials/lang.blade.php
+++ resources/views/layouts/partials/lang.blade.php
@@ -1,11 +1,16 @@
 <div class="control has-icons-left">
     <form action="{{ route('language.switch') }}" method="post">
         @csrf
+        @method('POST')
 
         <div class="select" id="lang">
-            <select onchange="this.form.submit()" name="language">
-                <option value="nl" {{ session('language') == 'nl' ? 'selected' : '' }}>Nederlands</option>
-                <option value="en" {{ session('language') == 'en' ? 'selected' : '' }}>English</option>
+            <select name="language" onchange="this.form.submit()">
+                <option value="nl" {{ session('language') == 'nl' ? 'selected' : '' }}>
+                    Nederlands
+                </option>
+                <option value="en" {{ session('language') == 'en' ? 'selected' : '' }}>
+                    English
+                </option>
             </select>
         </div>
         <span class="icon is-left">

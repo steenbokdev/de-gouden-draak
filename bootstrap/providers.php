433365d27b8fa8827e0d1b4cfc492f6f22b10cc0
Linde Rus <lsy.rus@student.avans.nl>
2024-06-12 23:14:21 +0200

    feat(dompdf): add

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Kyslik\ColumnSortable\ColumnSortableServiceProvider::class
+    Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
+    \Barryvdh\DomPDF\ServiceProvider::class,
 ];

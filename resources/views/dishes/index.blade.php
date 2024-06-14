6265a2412c5fe43c646da1bb885a655e0e9aca8b
Linde Rus <lsy.rus@student.avans.nl>
2024-06-14 21:49:26 +0200

    refactor(view): tabs

--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -16,8 +16,7 @@
 @section('addons-left')
     <form action="{{ route('dishes.index') }}" method="GET">
         <div class="field is-grouped">
-            <x-form.search id="search" type="text" placeholder="{{ __('dish/index.search') }}"
-                           value="{{ $searchQuery }}"/>
+            <x-form.search id="search" type="text" placeholder="{{ __('dish/index.search') }}" value="{{ $searchQuery }}"/>
 
             <div class="field is-grouped">
                 <x-form.select id="category">
@@ -56,74 +55,74 @@
     <div class="table-container">
         <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
-            <tr>
-                <th>
-                    @sortablelink('menu_number', __('dish/shared.menu_number'))
-                </th>
-                <th>
-                    @sortablelink('name', __('dish/shared.name'))
-                </th>
-                <th>
-                    {{ __('dish/shared.price') }}
-                </th>
-                <th>
-                    @sortablelink('category', __('dish/shared.category'))
-                </th>
-                <th>
-                    {{ __('dish/shared.description') }}
-                </th>
-                <th>
-                    {{ __('dish/index.actions.term') }}
-                </th>
-            </tr>
+                <tr>
+                    <th>
+                        @sortablelink('menu_number', __('dish/shared.menu_number'))
+                    </th>
+                    <th>
+                        @sortablelink('name', __('dish/shared.name'))
+                    </th>
+                    <th>
+                        {{ __('dish/shared.price') }}
+                    </th>
+                    <th>
+                        @sortablelink('category', __('dish/shared.category'))
+                    </th>
+                    <th>
+                        {{ __('dish/shared.description') }}
+                    </th>
+                    <th>
+                        {{ __('dish/index.actions.term') }}
+                    </th>
+                </tr>
             </thead>
 
             <tbody>
-            @forelse ($dishes as $dish)
-                <tr>
-                    <td>
-                        @isset($dish->menu_number)
-                            {{ $dish->menu_number }}.
-                        @endisset
+                @forelse ($dishes as $dish)
+                    <tr>
+                        <td>
+                            @isset($dish->menu_number)
+                                {{ $dish->menu_number }}.
+                            @endisset
 
-                        {{ $dish->menu_addition }}
-                    </td>
-                    <td>
-                        {{ $dish->name }}
-                    </td>
-                    <td>
-                        @isset($dish->price)
-                            @if(isset($dish->discount_price))
-                                <s>
-                                    &euro; {{ $dish->price }}
-                                </s>
-                                &euro; {{ $dish->discount_price }}
-                                @else
-                                    &euro; {{ $dish->price }}
-                            @endif
-                        @endisset
-                    </td>
-                    <td>
-                        {{ $dish->category }}
-                    </td>
-                    <td>
-                        {{ $dish->description }}
-                    </td>
-                    <td>
-                        <a href="{{ route('dishes.edit', $dish) }}">
-                            {{ __('dish/index.actions.edit') }}
-                        </a>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="6">
-                        <em>
-                            {{ __('dish/index.empty') }}
-                        </em>
-                    </td>
-                </tr>
-            @endforelse
+                            {{ $dish->menu_addition }}
+                        </td>
+                        <td>
+                            {{ $dish->name }}
+                        </td>
+                        <td>
+                            @isset($dish->price)
+                                @if(isset($dish->discount_price))
+                                    <s>
+                                        &euro; {{ $dish->price }}
+                                    </s>
+                                    &euro; {{ $dish->discount_price }}
+                                    @else
+                                        &euro; {{ $dish->price }}
+                                @endif
+                            @endisset
+                        </td>
+                        <td>
+                            {{ $dish->category }}
+                        </td>
+                        <td>
+                            {{ $dish->description }}
+                        </td>
+                        <td>
+                            <a href="{{ route('dishes.edit', $dish) }}">
+                                {{ __('dish/index.actions.edit') }}
+                            </a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6">
+                            <em>
+                                {{ __('dish/index.empty') }}
+                            </em>
+                        </td>
+                    </tr>
+                @endforelse
             </tbody>
         </table>
     </div>

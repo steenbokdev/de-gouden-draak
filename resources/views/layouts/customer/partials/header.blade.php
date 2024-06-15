2ae6a6643deb29f762f148242c09fe6ad63d63de
Linde Rus <lsy.rus@student.avans.nl>
2024-06-15 12:22:22 +0200

    refactor(header): inline styling

--- resources/views/layouts/customer/partials/header.blade.php
+++ resources/views/layouts/customer/partials/header.blade.php
@@ -1,25 +1,21 @@
-<table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
-    <tr style="height:50px;background-color:red">
-        <td style="text-align:center;width:30%;color:yellow;font-size:30px;">
-            <img style="vertical-align: middle;" src="{{ asset('images/dragon-small.png') }}" alt="Golden Dragon"
-                 height="50px">
+<table id="main_table" class="main-table">
+    <tr class="table-row">
+        <td class="table-cell">
+            <img src="{{ asset('images/dragon-small.png') }}" alt="Golden Dragon" height="50px">
             <span>De Gouden Draak</span>
-            <img style="vertical-align: middle;" src="{{ asset('images/dragon-small-flipped.png') }}"
-                 alt="Golden Dragon" height="50px">
+            <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="Golden Dragon" height="50px">
         </td>
-        <td style="overflow: hidden">
-            <a href="#" style="color:yellow;font-weight:bold;text-decoration: none;">
+        <td class="marquee-container">
+            <a href="#">
                 <div class="marquee">
                     Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!
                 </div>
             </a>
         </td>
-        <td style="text-align:center;width:30%;color:yellow;font-size:30px">
-            <img style="vertical-align: middle;" src="{{ asset('images/dragon-small.png') }}" alt="Golden Dragon"
-                 height="50px">
+        <td class="table-cell">
+            <img src="{{ asset('images/dragon-small.png') }}" alt="Golden Dragon" height="50px">
             <span>De Gouden Draak</span>
-            <img style="vertical-align: middle;" src="{{ asset('images/dragon-small-flipped.png') }}"
-                 alt="Golden Dragon" height="50px">
+            <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="Golden Dragon" height="50px">
         </td>
     </tr>
 </table>

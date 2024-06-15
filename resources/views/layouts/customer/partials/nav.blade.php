7241f8df8cf68efe37ccd7c0c9acb8f7651b4e1b
Linde Rus <lsy.rus@student.avans.nl>
2024-06-15 12:11:17 +0200

    refactor(view): buttons gradient

--- resources/views/layouts/customer/partials/nav.blade.php
+++ resources/views/layouts/customer/partials/nav.blade.php
@@ -7,20 +7,20 @@
                 <span style="font-size:40px;font-weight:bold;color:yellow">Chinees Indische Specialiteiten</span><br>
                 <span style="font-size:50px;font-weight:bold;color:yellow">De Gouden Draak</span><br>
             </p>
-            <table style="margin:auto;font-size:20px;color:white;border:1px solid white">
+            <table style="margin:auto;font-size:20px;color:white;">
                 <tr>
-                    <td style="vertical-align: middle;">
-                        <a href="{{ route('customer.menu') }}" style="color:white">
+                    <td style="vertical-align: middle;background-image: linear-gradient(#00F3FF, #165FE9);">
+                        <a href="{{ route('customer.menu') }}" style="color:white;text-decoration:none">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menukaart&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </a>
                     </td>
-                    <td style="vertical-align: middle;">
-                        <a href="{{ route('customer.news') }}" style="color:white">
+                    <td style="vertical-align: middle;background-image: linear-gradient(#00F3FF, #165FE9);">
+                        <a href="{{ route('customer.news') }}" style="color:white;text-decoration:none">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nieuws&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </a>
                     </td>
-                    <td style="vertical-align: middle;">
-                        <a href="{{ route('customer.contact') }}" style="color:white">
+                    <td style="vertical-align: middle;background-image: linear-gradient(#00F3FF, #165FE9);">
+                        <a href="{{ route('customer.contact') }}" style="color:white;text-decoration:none">
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </a>
                     </td>

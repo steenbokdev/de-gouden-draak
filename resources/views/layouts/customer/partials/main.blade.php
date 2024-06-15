d777b329a907b724681e88762573115041814d4c
Linde Rus <lsy.rus@student.avans.nl>
2024-06-15 20:30:14 +0200

    refactor(main): inline styling

--- resources/views/layouts/customer/partials/main.blade.php
+++ resources/views/layouts/customer/partials/main.blade.php
@@ -1,95 +1,92 @@
-<table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
-    <tr style="height:7px;background-color:red">
-        <td colspan="9">
-        </td>
+<table id="main_table" class="main-table">
+    <tr class="tr-padding">
+        <td colspan="9"/>
     <tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px"></td>
-        <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-left-top"/>
+        <td class="td-right-top"/>
+        <td class="td-right-bottom"/>
+        <td class="td-top-bottom"/>
+        <td class="td-left-bottom"/>
+        <td class="td-left-top"/>
+        <td class="td-right-top"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px">
-        <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-left-bottom"/>
+        <td class="td-all"/>
+        <td class="td-all"/>
+        <td/>
+        <td class="td-all"/>
+        <td class="td-all"/>
+        <td class="td-right-bottom"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px">
-        <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px"></td>
-        <td></td>
-        <td style="width:25px"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border-bottom:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-right-bottom"/>
+        <td class="td-all"/>
+        <td class="td-none"/>
+        <td/>
+        <td class="td-none"/>
+        <td class="td-all"/>
+        <td class="td-bottom"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:50px;background-color:red">
-        <td style="width:7px">
-        <td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
-        <td style="width:25px;"></td>
-        <td style="width:25px;"></td>
-        <td style="text-align:center">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-right-left"/>
+        <td class="td-none"/>
+        <td class="td-none"/>
+        <td class="center">
             @include('layouts.customer.partials.nav')
-            <div style="text-align:center">
-                <a style="color: yellow;text-decoration: none;" href="{{ route('customer.contact.new') }}">
+            <div class="center">
+                <a class="a-style" href="{{ route('customer.contact.new') }}">
                     {{ __('customer/layout.main.to-contact') }}
                 </a>
             </div>
         </td>
-        <td style="width:25px;"></td>
-        <td style="width:25px;"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
-        <td style="width:7px">
+        <td class="td-none"/>
+        <td class="td-none"/>
+        <td class="td-right-left"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px">
-        <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px"></td>
-        <td></td>
-        <td style="width:25px"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border-top:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-right-top"/>
+        <td class="td-all"/>
+        <td class="td-none"/>
+        <td/>
+        <td class="td-none"/>
+        <td class="td-all"/>
+        <td class="td-top"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px">
-        <td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-left-top"/>
+        <td class="td-all"/>
+        <td class="td-all"/>
+        <td/>
+        <td class="td-all"/>
+        <td class="td-all"/>
+        <td class="td-right-top"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:25px;background-color:red">
-        <td style="width:7px">
-        </td>
-        <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow"></td>
-        <td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-left:4px solid yellow;"></td>
-        <td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
-        <td style="width:7px">
+    <tr class="tr-base">
+        <td class="td-padding"/>
+        <td class="td-left-bottom"/>
+        <td class="td-right-bottom"/>
+        <td class="td-right"/>
+        <td class="td-top-bottom"/>
+        <td class="td-left"/>
+        <td class="td-left-bottom"/>
+        <td class="td-right-bottom"/>
+        <td class="td-padding"/>
     </tr>
-    <tr style="height:7px;background-color:red">
-        <td colspan="9">
-        </td>
+    <tr class="tr-padding">
+        <td colspan="9"/>
     <tr>
 </table>

0ba78669ae13f1601e0644bf7fc86bcb31ab6255
Linde Rus <lsy.rus@student.avans.nl>
2024-06-15 11:48:14 +0200

    wip(views): menu and news

--- resources/views/customer/menu.blade.php
+++ resources/views/customer/menu.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+@extends('layouts.customer.base')
+
+@section('content')
+    TODO: Linde, Voeg hier de inhoud van de menukaart toe. (perhaps de pdf?)
+@endsection
--- resources/views/customer/news.blade.php
+++ resources/views/customer/news.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+@extends('layouts.customer.base')
+
+@section('content')
+    <h3>
+        Door de Corona crisis is De Gouden Draak op het moment slechts beperkt open.
+        <br>
+        Het restaurant-gedeelte is gesloten. U kan uw favoriete gerechten nog wel afhalen.
+    </h3>
+    <br>
+@endsection

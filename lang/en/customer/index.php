fdbdbd164b07d5f66825943af827628ddd8573ee
Linde Rus <lsy.rus@student.avans.nl>
2024-06-15 19:54:48 +0200

    refactor(feedback): formatting

--- lang/en/customer/index.php
+++ lang/en/customer/index.php
@@ -3,7 +3,7 @@
 return [
     'title' => 'For years, De Gouden Draak has been a household name when it comes to the best take-away dishes in \'s-Hertogenbosch.',
     'sub_title' => 'We are happy to treat you to authentic dishes from the Cantonese kitchen.',
-    'discount_text' => 'From €:price to €:discount_price!',
+    'discount_text' => 'From € :price to € :discount_price!',
     'no_deals' => 'Unfortunately, there are no deals at the moment.',
     'deals' => 'Deals'
 ];
--- lang/nl/customer/contact.php
+++ lang/nl/customer/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal
-        station.',
+    'title' => 'De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen
+        achter het centraal station.',
     'room' => 'kamer',
 ];

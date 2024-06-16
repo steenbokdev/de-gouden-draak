7d45a2da3a4cc4c853ae8e7f9b8e0fced1560c3a
Lars <155370942+steenbokdev@users.noreply.github.com>
2024-06-16 21:07:33 +0200

    feat(sales): download receipt (#25)
    De kassabon kan worden gedownload onder de "Afrekenen" knop bij een
    kassabestelling.

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Notification;
 use App\Models\CheckoutOrder;
+use Barryvdh\DomPDF\PDF;
 use Carbon\Carbon;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Artisan;
@@ -57,5 +61,42 @@ class SalesController extends Controller
         ]));
 
         return $pdf->download("Verkoopcijfers_$date.pdf");
+    }
+
+    public function receipt()
+    {
+        $latest = CheckoutOrder::all('created_at')->sortByDesc('created_at')->first();
+
+        if ($latest === null) {
+            return redirect()->back()->with([
+                'notification' => [
+                    'type' => Notification::Danger,
+                    'body' => __('sales/download.no_orders')
+                ]
+            ]);
+        }
+
+        $sales = CheckoutOrder::where('created_at', $latest->created_at)->get();
+        $totalPrice = $sales->sum(function ($sale) {
+            return $sale->price_per_item * $sale->item_count;
+        });
+
+        $logoImage = base64_encode(file_get_contents(public_path('images/dragon-small.png')));
+        $logoImageFlipped = base64_encode(file_get_contents(public_path('images/dragon-small-flipped.png')));
+
+        $options = new Options();
+        $dompdf = new Dompdf($options);
+        $dompdf->setPaper([0, 0, 226.771, 283.464], 'portrait'); // Dimensions in mm (8cm x 10cm)
+        $dompdf->loadHtml(view('order.checkout.receipt', [
+            'sales' => $sales,
+            'totalPrice' => $totalPrice,
+            'logoImage' => $logoImage,
+            'logoImageFlipped' => $logoImageFlipped
+        ]));
+
+        $dompdf->render();
+
+        $date = $latest->created_at->format('dmY_His');
+        return $dompdf->stream("kassabon-$date.pdf");
     }
 }
